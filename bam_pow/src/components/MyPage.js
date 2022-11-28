06f257d31a56529f7707b99857431325f078f522
harrisimon <harrisimon@gmail.com>
2022-11-28 16:43:25 -0500

    added fav component

--- bam_pow/src/components/MyPage.js
+++ bam_pow/src/components/MyPage.js
@@ -2,7 +2,8 @@ import React from "react"
 import { Button, Card, Image, Container, Grid } from "semantic-ui-react"
 // import { Button } from 'semantic-ui-css'
 import comicBg from "../imgs/loginpage.svg"
-// import components for 
+// import components for
+import Favorites from "./Favorites"
 
 const MyPage = () => {
 	return (
@@ -12,7 +13,7 @@ const MyPage = () => {
 					<Grid.Column height={15} width={16}></Grid.Column>
 
 					<Grid.Column floated="left" width={7}>
-					<div className="comic-panel">
+						<div className="comic-panel">
 							<h1 className="comic-panel-font">My Collection</h1>
 							<p>All my read books</p>
 						</div>
@@ -21,6 +22,11 @@ const MyPage = () => {
 						<div className="comic-panel">
 							<h1 className="comic-panel-font">To Be Read</h1>
 							<p>Books I want to read</p>
+						</div>
+					</Grid.Column>
+					<Grid.Column floated="right" width={7}>
+						<div className="comic-panel">
+							<Favorites />
 						</div>
 					</Grid.Column>
 				</Grid>
